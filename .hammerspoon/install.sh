1819e7b7fc515c9167c2a3dde860a26a2d6a3e98
wang-q <wang-q@outlook.com>
2015-12-15 01:35:54 +0800

    Misc changes

--- .hammerspoon/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Hammerspoon configuring"
-
-HAMMERSPOONDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-find ${HOME} -type d -maxdepth 1 -name '.hammerspoon' | xargs rm -fr
-ln -Fs "$HAMMERSPOONDIR" ${HOME}/.hammerspoon
