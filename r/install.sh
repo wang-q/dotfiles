c9ccacf5fc151b52c38a377bf01680453d43be13
wang-q <wang-q@outlook.com>
2016-06-24 11:58:06 +0800

    Don't create temp .Rprofile

--- r/install.sh
+++ r/install.sh
@@ -4,15 +4,4 @@ RDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd ${RDIR}
 
-cat <<EOF > .Rprofile
-# set R mirror
-local({r <- getOption("repos")
-   r["CRAN"] <- "https://mirrors.tuna.tsinghua.edu.cn/CRAN"
-   options(repos=r)
-})
-
-EOF
-
 Rscript install.R
-
-rm .Rprofile
