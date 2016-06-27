d053269f1fae5ee3645a17cf517b180a056043bb
wang-q <wang-q@outlook.com>
2016-06-27 10:33:21 +0800

    Better mpv installation

--- brew/install.sh
+++ brew/install.sh
@@ -15,11 +15,6 @@ brew install cloc cmake htop-osx pandoc parallel sqlite tree
 # gtk+3
 brew install gsettings-desktop-schemas gtk+3 gnome-icon-theme
 
-# ffmpeg
-brew install lame libvo-aacenc x264 xvid
-brew install libass fdk-aac
-brew install ffmpeg --with-libass --with-fdk-aac
-
 # Graphics
 brew install freeglut gnuplot graphviz imagemagick plplot
 
@@ -30,13 +25,24 @@ brew install python
 pip install --upgrade pip setuptools
 brew install r --with-openblas
 
+# https://www.reddit.com/r/osx/comments/4ljbdq/mpv_tutorial_and_60_fps_playback_on_os_x/
+# ffmpeg
+brew install lame libvo-aacenc x264 xvid fdk-aac
+brew install --without-harfbuzz libass
+brew install --with-x265 --with-theora --with-rtmpdump --with-openssl \
+    --with-libvorbis --with-libass --with-libbs2b --with-rubberband --with-fdk-aac \
+    ffmpeg
+
+# mpv
+brew install duti
+brew install mvtools
+brew install ffms2
+brew install subliminal
+brew reinstall --with-vapoursynth --with-bundle mpv
+brew linkapps mpv
+
 # casks
 brew tap caskroom/cask
 brew cask install aegisub caffeine disk-inventory-x hammerspoon jabref
 brew cask install packer robomongo scroll-reverser sourcetree xmind
 brew cask install figtree dendroscope
-
-# mpv
-brew install duti
-brew reinstall mpv --with-bundle
-brew linkapps mpv
