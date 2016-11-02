4158c27d4a0b9b994668dd87e8d84d5de1799bc9
wang-q <wang-q@outlook.com>
2016-11-02 10:39:11 +0800

    Can't pass "repo=" to `font_install()` in `extrafont.R`

--- r/install.sh
+++ r/install.sh
@@ -5,3 +5,16 @@ BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${BASE_DIR}
 
 Rscript install.R
+
+cat <<EOF > .Rprofile
+# set R mirror
+local({r <- getOption("repos")
+   r["CRAN"] <- "https://mirrors.tuna.tsinghua.edu.cn/CRAN"
+   options(repos=r)
+})
+
+EOF
+
+Rscript extrafont.R
+
+rm .Rprofile
