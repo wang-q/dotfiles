5b85376e5fb233843e3d567ffb22d1e46509287d
wang-q <wang-q@outlook.com>
2016-11-05 20:50:16 +0800

    Move windows out

--- windows/choco.bat
+++
@@ -1,6 +0,0 @@
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-
-choco install -y dotnet3.5
-choco install -y vcredist2008 vcredist2010 vcredist2012 vcredist2013
-choco install -y curl wget
-choco install -y windirstat
