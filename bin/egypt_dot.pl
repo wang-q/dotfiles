7a0eb6e91cf46393f9244c7f192cc5194c409c4b
wang-q <wang-q@outlook.com>
2016-12-10 05:07:56 +0800

    Switch to Getopt::Long::Descriptive and Path::Tiny

--- bin/egypt_dot.pl
+++ bin/egypt_dot.pl
@@ -1,79 +1,74 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use strict;
 use warnings;
+use autodie;
 
-use Getopt::Long;
-use Pod::Usage;
-
-use Path::Class;
+use Getopt::Long::Descriptive;
+use Path::Tiny;
 
 #----------------------------------------------------------#
 # GetOpt section
 #----------------------------------------------------------#
-my $dir    = '.';
-my $suffix = '.expand';
-my $type   = 'png';
+my $description = <<'EOF';
+egypt_dot.pl - Draw C functions calling graph with egypt and dot.
 
-my $man  = 0;
-my $help = 0;
+http://www.gson.org/egypt/
 
-GetOptions(
-    'help|?'     => \$help,
-    'man'        => \$man,
-    'd|dir=s'    => \$dir,
-    's|suffix=s' => \$suffix,
-    't|type=s'   => \$type,
-) or pod2usage(2);
+    cpanm http://www.gson.org/egypt/download/egypt-1.10.tar.gz
 
-pod2usage(1) if $help;
-pod2usage( -exitstatus => 0, -verbose => 2 ) if $man;
+    make clean
+    make CFLAGS=-fdump-rtl-expand CC=gcc-6
+    egypt_dot.pl -d .
+    rm *.dot *.expand
+
+Usage: perl %c [options]
+EOF
+
+(
+    #@type Getopt::Long::Descriptive::Opts
+    my $opt,
+
+    #@type Getopt::Long::Descriptive::Usage
+    my $usage,
+    )
+    = Getopt::Long::Descriptive::describe_options(
+    $description,
+    [ 'help|h', 'display this message' ],
+    [],
+    [ 'dir|d=s',    'where to find the .expand files', { default => "." }, ],
+    [ 'suffix|s=s', 'alternative suffix',              { default => ".expand" }, ],
+    [ 'type|t=s',   'output file format',              { default => "png" } ],
+    { show_defaults => 1, }
+    );
+
+$usage->die if $opt->{help};
+
+if ( !path( $opt->{dir} )->is_dir ) {
+    $usage->die( { pre_text => "The input dir [$opt->{dir}] doesn't exist." } );
+}
 
 #----------------------------------------------------------#
 # run
 #----------------------------------------------------------#
-$dir = dir($dir)->absolute;
-while ( my $file = $dir->next ) {
-    next unless -f $file;
 
-    my $basename = $file->basename;
-    my $suffix_index = index $basename, $suffix;
+my $iter = path( $opt->{dir} )->iterator( { follow_symlinks => 0, } );
+while (
+    #@type Path::Tiny
+    my $path = $iter->()
+    )
+{
+    next unless $path->is_file;
+
+    my $basename = $path->basename;
+    my $suffix_index = index $basename, $opt->{suffix};
     next unless $suffix_index >= 0;
     $basename = substr $basename, 0, $suffix_index;
 
-    print "Process $file\n";
-    my $dot_file   = file( $dir, $basename . '.dot' );
-    my $graph_file = file( $dir, $basename . ".$type" );
-    system "egypt $file > $dot_file";
-    system "dot $dot_file -T$type > $graph_file";
+    print "Process $path\n";
+    my $dot_file   = path( $opt->{dir}, $basename . '.dot' );
+    my $graph_file = path( $opt->{dir}, $basename . ".$opt->{type}" );
+    system "egypt $path > $dot_file";
+    system "dot $dot_file -T$opt->{type} > $graph_file";
 }
 
 __END__
-
-=head1 NAME
-
-egypt_dot.pl - Draw C functions calling graph with egypt and dot
-
-=head1 SYNOPSIS
-
-    perl egypt_dot.pl [options] [file ...]
-      Options:
-        --help              brief help message
-        --man               full documentation
-        -d, --dir           where to find the .c.01.rtl files
-        -s, --suffix        alternative suffix (GCC 4)
-        -t, --type          output file format (.png)
-
-    make clean
-    make CFLAGS=-fdump-rtl-expand
-    perl ~/Scripts/tool/egypt_dot.pl -d .
-    rm *.dot *.expand
-
-=cut
-
-=head1 egypt
-
-http://www.gson.org/egypt/
-
-cpanm http://www.gson.org/egypt/download/egypt-1.10.tar.gz
-
-=cut
