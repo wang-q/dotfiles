fca9ec837d79d8ecd2310acb473deeef7902cec3
wang-q <wang-q@outlook.com>
2018-04-12 02:41:10 +0800

    Add some bioconductor packages

--- r/install.R
+++ r/install.R
@@ -19,7 +19,7 @@ for(library in c(basic_libraries, graphics_libraries, bio_libraries ) ) {
 }
 
 # bioconductor packages
-bioC_libraries <- c("biomaRt")
+bioC_libraries <- c("biomaRt", "hthgu133a.db", "hgu133a2.db", "IlluminaHumanMethylation27k.db", "IlluminaHumanMethylation450k.db")
 
 source("http://www.bioconductor.org/biocLite.R")
 for(library in bioC_libraries) {
