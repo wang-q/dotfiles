0e7cc0f314b270698837af4557ef2aa611eb0eee
wang-q <wang-q@outlook.com>
2018-11-30 23:35:17 +0800

    PYPI_MIRROR

--- python/install.sh
+++ python/install.sh
@@ -8,10 +8,12 @@ hash python 2>/dev/null || {
 
 cd ${BASE_DIR}
 
-pip3 install --upgrade pip setuptools
+PYPI_MIRROR=https://mirrors.ustc.edu.cn/pypi/web/simple
 
-pip3 install numpy matplotlib
-pip3 install pandas sympy
-pip3 install jupyter scipy
-pip3 install lxml statsmodels patsy
-pip3 install beautifulsoup4 scikit-learn seaborn
+pip3 install -i ${PYPI_MIRROR} --upgrade pip setuptools
+
+pip3 install -i ${PYPI_MIRROR} numpy matplotlib
+pip3 install -i ${PYPI_MIRROR} pandas sympy
+pip3 install -i ${PYPI_MIRROR} jupyter scipy
+pip3 install -i ${PYPI_MIRROR} lxml statsmodels patsy
+pip3 install -i ${PYPI_MIRROR} beautifulsoup4 scikit-learn seaborn
