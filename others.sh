4baec17e9663942ad1c272c2a515fd4f56bc18a5
wang-q <wang-q@outlook.com>
2019-08-05 12:12:26 +0800

    Separate from genomics.sh

---
+++ others.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/share/
+
+echo "==> trinity 2.6.6"
+brew install salmon
+
+#brew install trinity
+wget -N -P /tmp https://github.com/trinityrnaseq/trinityrnaseq/archive/Trinity-v2.6.6.tar.gz
+
+cd $HOME/share/
+tar xvfz /tmp/Trinity-v2.6.6.tar.gz
+
+cd $HOME/share/trinityrnaseq-Trinity-v2.6.6
+make
+make plugins
+
+sed -i".bak" 's/::Bin/::RealBin/' $HOME/share/trinityrnaseq-Trinity-v2.6.6/Trinity
+ln -fs $HOME/share/trinityrnaseq-Trinity-v2.6.6/Trinity $HOME/bin/Trinity
+
+echo "==> gatk 3.5"
+# brew install maven
+
+if [[ ! -e /tmp/gatk-3.5.tar.gz ]]; then
+    wget -N -P /tmp https://github.com/broadgsa/gatk-protected/archive/3.5.tar.gz
+
+    cd $HOME/share/
+    tar xvfz /tmp/3.5.tar.gz
+
+    cd $HOME/share/gatk-protected-*
+    # Compile the GATK but not Queue
+    mvn verify -P\!queue
+    mv target/executable $HOME/share/gatk
+
+    cd $HOME/share
+    tar cvfz gatk-3.5.tar.gz gatk
+    mv gatk-3.5.tar.gz /tmp/
+else
+    cd $HOME/share/
+    tar xvfz /tmp/gatk-3.5.tar.gz
+fi
+
+cd $HOME/share/
+java -jar java -jar gatk/GenomeAnalysisTK.jar --help
+
+echo "==> interproscan"
+cd /tmp/
+
+wget -N -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.32-71.0/interproscan-5.32-71.0-64-bit.tar.gz
+wget -N -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.32-71.0/interproscan-5.32-71.0-64-bit.tar.gz.md5
+
+# md5sum -c interproscan-5.32-71.0-64-bit.tar.gz.md5
+
+wget -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/data/panther-data-12.0.tar.gz
+wget -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/data/panther-data-12.0.tar.gz.md5
+
+# md5sum -c panther-data-12.0.tar.gz.md5
+
+# wget -N -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/lookup_service/lookup_service_5.32-71.0.tar.gz
+# wget -N -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/lookup_service/lookup_service_5.32-71.0.tar.gz.md5
+
+# md5sum -c lookup_service_5.32-71.0.tar.gz.md5
+
+cd $HOME/share/
+
+rm -fr interproscan
+
+pigz -dc /tmp/interproscan-5.32-71.0-64-bit.tar.gz | pv | tar -pxv -f -
+pigz -dc /tmp/panther-data-12.0.tar.gz | pv | tar -pxv -f -
+
+mv interproscan-5.32-71.0 interproscan
+mv panther interproscan/data
+
+# turn off local pre-calculated match lookup service
+sed -i '/precalculated.match.lookup.service.url/s/^/#/g' interproscan/interproscan.properties
+
+ln -fs $HOME/share/interproscan/interproscan.sh $HOME/bin/interproscan.sh
+
+# Manually download SignalP, TMHMM
+tar -xvz -f /tmp/signalp-4.1f.Linux.tar.gz
+mkdir -p interproscan/bin/signalp/4.1/
+cp -R signalp-4.1/* interproscan/bin/signalp/4.1/
+sed -i '/\$ENV{SIGNALP} =/c\'"\$ENV{SIGNALP} = 'bin/signalp/4.1';" interproscan/bin/signalp/4.1/signalp
+
+tar -xvz -f /tmp/tmhmm-2.0c.Linux.tar.gz
+mkdir -p interproscan/bin/tmhmm/2.0c/
+cp tmhmm-2.0c/bin/decodeanhmm.Linux_x86_64 interproscan/bin/tmhmm/2.0c/decodeanhmm
+mkdir -p interproscan/data/tmhmm/2.0c/
+cp tmhmm-2.0c/lib/TMHMM2.0.model interproscan/data/tmhmm/2.0c/TMHMM2.0c.model
