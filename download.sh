d2c369eb8b7c01301c70205645bf488ee50e8443
wang-q <wang-q@outlook.com>
2019-08-05 09:14:11 +0800

    Separate codes from genomics.sh to download.sh.
    Don't use /prepare/resource

---
+++ download.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+echo "====> Download Genomics related tools <===="
+
+mkdir -p $HOME/bin
+mkdir -p $HOME/share
+mkdir -p $HOME/Scripts
+
+# make sure $HOME/bin in your $PATH
+if grep -q -i homebin $HOME/.bashrc; then
+    echo "==> .bashrc already contains homebin"
+else
+    echo "==> Update .bashrc"
+
+    HOME_PATH='export PATH="$HOME/bin:$PATH"'
+    echo '# Homebin' >> $HOME/.bashrc
+    echo $HOME_PATH >> $HOME/.bashrc
+    echo >> $HOME/.bashrc
+
+    eval $HOME_PATH
+fi
+
+# Clone or pull other repos
+for OP in dotfiles alignDB withncbi; do
+    if [[ ! -d "$HOME/Scripts/$OP/.git" ]]; then
+        if [[ ! -d "$HOME/Scripts/$OP" ]]; then
+            echo "==> Clone $OP"
+            git clone https://github.com/wang-q/${OP}.git "$HOME/Scripts/$OP"
+        else
+            echo "==> $OP exists"
+        fi
+    else
+        echo "==> Pull $OP"
+        pushd "$HOME/Scripts/$OP" > /dev/null
+        git pull
+        popd > /dev/null
+    fi
+done
+
+# alignDB
+chmod +x $HOME/Scripts/alignDB/alignDB.pl
+ln -fs $HOME/Scripts/alignDB/alignDB.pl $HOME/bin/alignDB.pl
+
+echo "==> Jim Kent bin"
+cd $HOME/bin/
+if [[ $(uname) == 'Darwin' ]]; then
+    JKBIN_TAR_GZ=/tmp/jkbin-darwin-2011.tar.gz
+    if [ ! -e ${JKBIN_TAR_GZ} ]; then
+        wget -N -P /tmp https://github.com/egateam/egavm/releases/download/20170907/jkbin-darwin-2011.tar.gz
+    fi
+else
+    JKBIN_TAR_GZ=/tmp/jkbin-ubuntu-1404-2011.tar.gz
+    if [ ! -e ${JKBIN_TAR_GZ} ]; then
+        wget -N -P /tmp https://github.com/egateam/egavm/releases/download/20170907/jkbin-ubuntu-1404-2011.tar.gz
+    fi
+fi
+
+echo "==> untar from ${JKBIN_TAR_GZ}"
+tar xvfz ${JKBIN_TAR_GZ} x86_64/axtChain
+tar xvfz ${JKBIN_TAR_GZ} x86_64/axtSort
+tar xvfz ${JKBIN_TAR_GZ} x86_64/axtToMaf
+tar xvfz ${JKBIN_TAR_GZ} x86_64/chainAntiRepeat
+tar xvfz ${JKBIN_TAR_GZ} x86_64/chainMergeSort
+tar xvfz ${JKBIN_TAR_GZ} x86_64/chainNet
+tar xvfz ${JKBIN_TAR_GZ} x86_64/chainPreNet
+tar xvfz ${JKBIN_TAR_GZ} x86_64/chainSplit
+tar xvfz ${JKBIN_TAR_GZ} x86_64/chainStitchId
+tar xvfz ${JKBIN_TAR_GZ} x86_64/faToTwoBit
+tar xvfz ${JKBIN_TAR_GZ} x86_64/lavToPsl
+tar xvfz ${JKBIN_TAR_GZ} x86_64/netChainSubset
+tar xvfz ${JKBIN_TAR_GZ} x86_64/netFilter
+tar xvfz ${JKBIN_TAR_GZ} x86_64/netSplit
+tar xvfz ${JKBIN_TAR_GZ} x86_64/netSyntenic
+tar xvfz ${JKBIN_TAR_GZ} x86_64/netToAxt
+
+mv $HOME/bin/x86_64/* $HOME/bin/
+
+echo "==> circos"
+wget -N -P /tmp http://circos.ca/distribution/circos-0.69-6.tgz
+wget -N -P /tmp http://circos.ca/distribution/circos-tools-0.22.tgz
+
+cd $HOME/share/
+rm -fr circos
+tar xvfz /tmp/circos-0.69-6.tgz
+mv circos-0.69-6 circos
+
+perl -pi -e 's{^#!\/bin\/env}{#!\/usr\/bin\/env}g' $HOME/share/circos/bin/circos
+perl -pi -e 's{^#!\/bin\/env}{#!\/usr\/bin\/env}g' $HOME/share/circos/bin/gddiag
+
+ln -fs $HOME/share/circos/bin/circos $HOME/bin/circos
+
+cd $HOME/share/
+rm -fr circos-tools
+tar xvfz /tmp/circos-tools-0.22.tgz
+mv circos-tools-0.22 circos-tools
--- genomics.sh
+++ genomics.sh
@@ -5,20 +5,6 @@ echo "====> Building Genomics related tools <===="
 mkdir -p $HOME/bin
 mkdir -p $HOME/share
 
-# make sure $HOME/bin in your $PATH
-if grep -q -i homebin $HOME/.bashrc; then
-    echo "==> .bashrc already contains homebin"
-else
-    echo "==> Update .bashrc"
-
-    HOME_PATH='export PATH="$HOME/bin:$PATH"'
-    echo '# Homebin' >> $HOME/.bashrc
-    echo $HOME_PATH >> $HOME/.bashrc
-    echo >> $HOME/.bashrc
-
-    eval $HOME_PATH
-fi
-
 echo "==> anchr"
 curl -fsSL https://raw.githubusercontent.com/wang-q/App-Anchr/master/share/install_dep.sh | bash
 
@@ -46,11 +32,10 @@ echo "==> trinity 2.6.6"
 brew install salmon
 
 #brew install trinity
-cd /prepare/resource/
-wget -N https://github.com/trinityrnaseq/trinityrnaseq/archive/Trinity-v2.6.6.tar.gz
+wget -N -P /tmp https://github.com/trinityrnaseq/trinityrnaseq/archive/Trinity-v2.6.6.tar.gz
 
 cd $HOME/share/
-tar xvfz /prepare/resource/Trinity-v2.6.6.tar.gz
+tar xvfz /tmp/Trinity-v2.6.6.tar.gz
 
 cd $HOME/share/trinityrnaseq-Trinity-v2.6.6
 make
@@ -62,13 +47,11 @@ ln -fs $HOME/share/trinityrnaseq-Trinity-v2.6.6/Trinity $HOME/bin/Trinity
 echo "==> gatk 3.5"
 # brew install maven
 
-if [[ ! -e /prepare/resource/gatk-3.5.tar.gz ]];
-then
-    cd /prepare/resource/
-    wget -N https://github.com/broadgsa/gatk-protected/archive/3.5.tar.gz
+if [[ ! -e /tmp/gatk-3.5.tar.gz ]]; then
+    wget -N -P /tmp https://github.com/broadgsa/gatk-protected/archive/3.5.tar.gz
 
     cd $HOME/share/
-    tar xvfz /prepare/resource/3.5.tar.gz
+    tar xvfz /tmp/3.5.tar.gz
 
     cd $HOME/share/gatk-protected-*
     # Compile the GATK but not Queue
@@ -77,50 +60,31 @@ then
 
     cd $HOME/share
     tar cvfz gatk-3.5.tar.gz gatk
-    mv gatk-3.5.tar.gz /prepare/resource/
+    mv gatk-3.5.tar.gz /tmp/
 else
     cd $HOME/share/
-    tar xvfz /prepare/resource/gatk-3.5.tar.gz
+    tar xvfz /tmp/gatk-3.5.tar.gz
 fi
 
 cd $HOME/share/
 java -jar java -jar gatk/GenomeAnalysisTK.jar --help
 
-echo "==> circos"
-cd /prepare/resource/
-wget -N http://circos.ca/distribution/circos-0.69-6.tgz
-wget -N http://circos.ca/distribution/circos-tools-0.22.tgz
-
-cd $HOME/share/
-rm -fr circos
-tar xvfz /prepare/resource/circos-0.69-6.tgz
-mv circos-0.69-6 circos
-
-perl -pi -e 's{^#!\/bin\/env}{#!\/usr\/bin\/env}g' $HOME/share/circos/bin/circos
-perl -pi -e 's{^#!\/bin\/env}{#!\/usr\/bin\/env}g' $HOME/share/circos/bin/gddiag
-
-ln -fs $HOME/share/circos/bin/circos $HOME/bin/circos
-
-cd $HOME/share/
-rm -fr circos-tools
-tar xvfz /prepare/resource/circos-tools-0.22.tgz
-mv circos-tools-0.22 circos-tools
 
 echo "==> interproscan"
-cd /prepare/resource/
+cd /tmp/
 
-wget -N ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.32-71.0/interproscan-5.32-71.0-64-bit.tar.gz
-wget -N ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.32-71.0/interproscan-5.32-71.0-64-bit.tar.gz.md5
+wget -N -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.32-71.0/interproscan-5.32-71.0-64-bit.tar.gz
+wget -N -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.32-71.0/interproscan-5.32-71.0-64-bit.tar.gz.md5
 
-md5sum -c interproscan-5.32-71.0-64-bit.tar.gz.md5
+# md5sum -c interproscan-5.32-71.0-64-bit.tar.gz.md5
 
-wget ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/data/panther-data-12.0.tar.gz
-wget ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/data/panther-data-12.0.tar.gz.md5
+wget -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/data/panther-data-12.0.tar.gz
+wget -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/data/panther-data-12.0.tar.gz.md5
 
 md5sum -c panther-data-12.0.tar.gz.md5
 
-# wget ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/lookup_service/lookup_service_5.32-71.0.tar.gz
-# wget ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/lookup_service/lookup_service_5.32-71.0.tar.gz.md5
+# wget -N -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/lookup_service/lookup_service_5.32-71.0.tar.gz
+# wget -N -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/lookup_service/lookup_service_5.32-71.0.tar.gz.md5
 
 # md5sum -c lookup_service_5.32-71.0.tar.gz.md5
 
@@ -128,8 +92,8 @@ cd $HOME/share/
 
 rm -fr interproscan
 
-pigz -dc /prepare/resource/interproscan-5.32-71.0-64-bit.tar.gz | pv | tar -pxv -f -
-pigz -dc /prepare/resource/panther-data-12.0.tar.gz | pv | tar -pxv -f -
+pigz -dc /tmp/interproscan-5.32-71.0-64-bit.tar.gz | pv | tar -pxv -f -
+pigz -dc /tmp/panther-data-12.0.tar.gz | pv | tar -pxv -f -
 
 mv interproscan-5.32-71.0 interproscan
 mv panther interproscan/data
@@ -140,34 +104,13 @@ sed -i '/precalculated.match.lookup.service.url/s/^/#/g' interproscan/interprosc
 ln -fs $HOME/share/interproscan/interproscan.sh $HOME/bin/interproscan.sh
 
 # Manually download SignalP, TMHMM
-tar -xvz -f /prepare/resource/signalp-4.1f.Linux.tar.gz
+tar -xvz -f /tmp/signalp-4.1f.Linux.tar.gz
 mkdir -p interproscan/bin/signalp/4.1/
 cp -R signalp-4.1/* interproscan/bin/signalp/4.1/
 sed -i '/\$ENV{SIGNALP} =/c\'"\$ENV{SIGNALP} = 'bin/signalp/4.1';" interproscan/bin/signalp/4.1/signalp
 
-tar -xvz -f /prepare/resource/tmhmm-2.0c.Linux.tar.gz
+tar -xvz -f /tmp/tmhmm-2.0c.Linux.tar.gz
 mkdir -p interproscan/bin/tmhmm/2.0c/
 cp tmhmm-2.0c/bin/decodeanhmm.Linux_x86_64 interproscan/bin/tmhmm/2.0c/decodeanhmm
 mkdir -p interproscan/data/tmhmm/2.0c/
 cp tmhmm-2.0c/lib/TMHMM2.0.model interproscan/data/tmhmm/2.0c/TMHMM2.0c.model
-
-echo "==> clone or pull"
-mkdir -p $HOME/Scripts/
-
-for OP in dotfiles alignDB withncbi; do
-    if [[ ! -d "$HOME/Scripts/$OP/.git" ]]; then
-        if [[ ! -d "$HOME/Scripts/$OP" ]]; then
-            echo "==> Clone $OP"
-            git clone https://github.com/wang-q/${OP}.git "$HOME/Scripts/$OP"
-        else
-            echo "==> $OP exists"
-        fi
-    else
-        echo "==> Pull $OP"
-        cd "$HOME/Scripts/$OP"
-        git pull
-    fi
-done
-
-chmod +x $HOME/Scripts/alignDB/alignDB.pl
-ln -fs $HOME/Scripts/alignDB/alignDB.pl $HOME/bin/alignDB.pl
