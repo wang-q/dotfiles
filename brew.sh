e79163240f3def1cd5e5e744900d420eadfab7e8
Qiang Wang <wang-q@users.noreply.github.com>
2019-08-19 06:48:53 +0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -110,7 +110,7 @@ brew install berkeley-db gd gsl libffi libgit2 libxml2 libxslt pcre readline sql
 # brew link --force libffi
 
 # other tools
-brew install gpg2 stow
+brew install gpg2 screen stow
 brew install cloc cmake htop pandoc parallel pigz tldr tree
 brew install jq jo pup datamash miller tsv-utils
 
