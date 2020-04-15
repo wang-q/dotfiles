39552157f1edd40d1837300d778b4a98814d7113
wang-q <wang-q@outlook.com>
2020-04-16 02:39:59 +0800

    mas

--- brewcask.sh
+++ brewcask.sh
@@ -34,3 +34,18 @@ brew cask install neteasemusic youdaodict
 
 # versions
 brew cask install homebrew/cask-versions/microsoft-remote-desktop-beta
+
+# mas
+brew install mas
+
+mas install 425264550 # Disk Speed Test (3.2)
+mas install 451108668 # QQ (6.6.2)
+mas install 491854842 # YoudaoDict (2.7.0)
+mas install 803453959 # Slack (4.4.2)
+mas install 836500024 # WeChat (2.3.31)
+mas install 880001334 # Reeder (3.2.3)
+mas install 944848654 # NeteaseMusic (2.3.2)
+mas install 1012296988 # 爱奇艺 (5.17.6)
+mas install 1243368435 # 讯飞语音输入 (1.1.1224)
+mas install 1344409961 # BeyondcowTXTReader (1.9.6)
+mas install 1435447041 # DingTalk (5.0.2)
