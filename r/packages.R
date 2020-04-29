a9b932bd63a60c507f135d1d608413eeefd7e69d
wang-q <wang-q@outlook.com>
2020-04-29 13:33:19 +0800

    bioc 3.10

--- r/packages.R
+++ r/packages.R
@@ -18,6 +18,7 @@ for(library in c(basic_libraries, graphics_libraries, stat_libraries, bio_librar
         install.packages(library, repos="https://mirrors.nju.edu.cn/CRAN")
     }
 }
+BiocManager::install(version = "3.10", ask = FALSE)
 
 # bioconductor packages
 bioC_libraries <- c("biomaRt", "GenomicDataCommons", "bsseq", "DSS", "scran", "scater", "edgeR", "pheatmap", "vcd", "monocle", "GenomeInfoDbData", "DESeq2", "clusterProfiler", "factoextra")
@@ -25,6 +26,6 @@ bioC_anno <- c("AnnotationDbi", "org.Hs.eg.db", "org.Rn.eg.db", "hthgu133a.db", 
 
 for(library in c( bioC_libraries, bioC_anno ) ) {
     if(!is_installed(library)) {
-        BiocManager::install(library, update=FALSE, version = "3.9")
+        BiocManager::install(library, update=FALSE, version = "3.10")
     }
 }
