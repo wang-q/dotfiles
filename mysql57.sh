a567f779d6d0ed4c13a4e7c32e3221be10639cc8
wang-q <wang-q@outlook.com>
2020-09-17 13:10:27 +0800

    mysql via brew

---
+++ mysql57.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+echo "==> Install Mysql@5.7"
+brew install mysql@5.7
+brew link mysql@5.7 --force
+
+echo "==> Start mysql service"
+mysqld_safe &
+sleep 5
+
+echo "==> Adjust mysql"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'alignDB'@'%' IDENTIFIED BY 'alignDB'"
+mysql -uroot -e "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))"
+
+cat <<EOF
+
+# normal startup
+mysqld_safe &
+
+# shutdown
+mysqladmin shutdown -uroot
+
+# DBD::mysql
+cpanm --notest DBD::mysql
+
+# If the above command failed, use the following
+cpanm --look DBD::mysql
+mkdir -p /tmp/mysql-static && cp $(brew --prefix)/opt/mysql@5.7/lib/*.a /tmp/mysql-static
+perl Makefile.PL \
+  --testuser alignDB \
+  --testpassword alignDB \
+  --cflags="-I$(brew --prefix)/opt/mysql@5.7/include/mysql -fPIC -DUNIV_LINUX -DUNIV_LINUX" \
+  --libs="-L/tmp/mysql-static -lmysqlclient -lz -lm"
+make test
+make install
+
+EOF
