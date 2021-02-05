aaa0ef2f618f518bb1d5c5e737cd8c3d193d56f4
wang-q <wang-q@outlook.com>
2021-02-05 17:23:28 +0800

    interproscan-5.48-83.0

--- others.sh
+++ others.sh
@@ -22,15 +22,15 @@ echo "==> interproscan"
 mkdir -p $HOME/software/interproscan
 cd $HOME/software/interproscan
 
-proxychains4 wget -N ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.32-71.0/interproscan-5.32-71.0-64-bit.tar.gz
-wget -N ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.32-71.0/interproscan-5.32-71.0-64-bit.tar.gz.md5
+proxychains4 wget -N ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.48-83.0/interproscan-5.48-83.0-64-bit.tar.gz
+wget ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.48-83.0/interproscan-5.48-83.0-64-bit.tar.gz.md5
 
-md5sum -c interproscan-5.32-71.0-64-bit.tar.gz.md5
+md5sum -c interproscan-5.48-83.0-64-bit.tar.gz.md5
 
-proxychains4 wget -N ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/data/panther-data-12.0.tar.gz
-wget ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/data/panther-data-12.0.tar.gz.md5
+# proxychains4 wget -N ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/data/panther-data-12.0.tar.gz
+# wget ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/data/panther-data-12.0.tar.gz.md5
 
-md5sum -c panther-data-12.0.tar.gz.md5
+# md5sum -c panther-data-12.0.tar.gz.md5
 
 # wget -N -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/lookup_service/lookup_service_5.32-71.0.tar.gz
 # wget -N -P /tmp ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/lookup_service/lookup_service_5.32-71.0.tar.gz.md5
@@ -41,11 +41,11 @@ cd $HOME/share/
 
 rm -fr interproscan
 
-pigz -dc /$HOME/software/interproscan/interproscan-5.32-71.0-64-bit.tar.gz | pv | tar -pxv -f -
-mv interproscan-5.32-71.0 interproscan
+pigz -dc /$HOME/software/interproscan/interproscan-5.48-83.0-64-bit.tar.gz | pv | tar -pxv -f -
+mv interproscan-5.48-83.0 interproscan
 
-pigz -dc /$HOME/software/interproscan/panther-data-12.0.tar.gz | pv | tar -pxv -f -
-mv panther interproscan/data
+# pigz -dc /$HOME/software/interproscan/panther-data-12.0.tar.gz | pv | tar -pxv -f -
+# mv panther interproscan/data
 
 # turn off local pre-calculated match lookup service
 sed -i '/precalculated.match.lookup.service.url/s/^/#/g' interproscan/interproscan.properties
@@ -65,3 +65,6 @@ mkdir -p interproscan/bin/tmhmm/2.0c/
 cp tmhmm-2.0c/bin/decodeanhmm.Linux_x86_64 interproscan/bin/tmhmm/2.0c/decodeanhmm
 mkdir -p interproscan/data/tmhmm/2.0c/
 cp tmhmm-2.0c/lib/TMHMM2.0.model interproscan/data/tmhmm/2.0c/TMHMM2.0c.model
+
+cd $HOME/share/interproscan
+python3 initial_setup.py
