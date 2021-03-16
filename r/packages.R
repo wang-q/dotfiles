aa62562cf6601f8d57cbeaf6b425a2f90c80c8c7
wang-q <wang-q@outlook.com>
2021-03-16 11:30:32 +0800

    Update packages.R

--- r/packages.R
+++ r/packages.R
@@ -8,9 +8,9 @@ is_installed <- function(package) {
 }
 
 # CRAN packages
-basic_libraries <- c("devtools", "tidyverse", "reshape2", "getopt", "doParallel")
-graphics_libraries <- c("scales", "gridExtra", "knitr", "rmarkdown", "kableExtra", "extrafont", "tikzDevice", "pander")
-stat_libraries <- c("survival", "randomForestSRC", "pROC", "verification", "survminer", "VennDiagram")
+basic_libraries <- c("devtools", "tidyverse", "reshape2", "getopt", "foreach", "doParallel")
+graphics_libraries <- c("scales", "gridExtra", "knitr", "rmarkdown", "kableExtra", "extrafont", "ggplot2", "tikzDevice", "pander")
+stat_libraries <- c("survival", "randomForestSRC", "pROC", "verification", "timeROC", "survminer", "VennDiagram")
 bio_libraries <- c("BiocManager", "ape", "adephylo", "genetics", "poppr", "taxize", "brranching")
 
 for(library in c(basic_libraries, graphics_libraries, stat_libraries, bio_libraries ) ) {
@@ -22,7 +22,7 @@ BiocManager::install(version = "3.12", ask = FALSE)
 
 # bioconductor packages
 bioC_libraries <- c("biomaRt", "GenomicDataCommons", "GEOquery", "bsseq", "DSS", "scran", "scater", "edgeR", "pheatmap", "vcd", "monocle", "GenomeInfoDbData", "DESeq2", "clusterProfiler", "factoextra")
-bioC_anno <- c("AnnotationDbi", "org.Hs.eg.db", "org.Rn.eg.db", "hthgu133a.db", "hgu133a2.db", "IlluminaHumanMethylation27k.db", "IlluminaHumanMethylation450k.db")
+bioC_anno <- c("AnnotationDbi", "org.Hs.eg.db", "org.Rn.eg.db", "hthgu133a.db", "hgu133a2.db")
 
 for(library in c( bioC_libraries, bioC_anno ) ) {
     if(!is_installed(library)) {
