83c80c2f4320b16c89540c55c4d498155e12615e
Qiang Wang <wang-q@outlook.com>
2022-02-26 12:31:25 +0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -92,7 +92,7 @@ brew install berkeley-db gd gsl libffi libgit2 libxml2 libxslt pcre readline sql
 brew install screen stow
 brew install cloc cmake htop parallel pigz tldr tree pv
 brew install jq pup datamash miller tsv-utils
-brew install hyperfine ripgrep
+brew install hyperfine ripgrep tiv
 
 # large packages
 if [[ "$OSTYPE" == "darwin"* ]]; then
