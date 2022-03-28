8b122a8677b4918f520aceea759df3adc62575f2
wang-q <wang-q@outlook.com>
2022-03-29 05:15:49 +0800

    poetry

--- python/install.sh
+++ python/install.sh
@@ -4,6 +4,7 @@ BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd "${BASE_DIR}" || exit
 
+# pip
 PYPI_MIRROR=https://pypi.tuna.tsinghua.edu.cn/simple
 
 #pip3 install -i ${PYPI_MIRROR} --upgrade pip setuptools
@@ -13,3 +14,6 @@ pip3 install -i ${PYPI_MIRROR} pandas sympy
 pip3 install -i ${PYPI_MIRROR} jupyter scipy
 pip3 install -i ${PYPI_MIRROR} lxml statsmodels patsy
 pip3 install -i ${PYPI_MIRROR} beautifulsoup4 scikit-learn seaborn
+
+# poetry can search packages
+curl -sSL https://install.python-poetry.org | python3 -
