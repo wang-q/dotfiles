13524ace69b901e906e78b93816dbc4a5205c1cb
wang-q <wang-q@outlook.com>
2022-04-03 03:32:49 +0800

    Update genomics.sh

--- genomics.sh
+++ genomics.sh
@@ -13,36 +13,36 @@ brew tap brewsci/bio
 brew tap brewsci/science
 
 brew install clustal-w mafft muscle
-brew install lastz raxml fasttree
-brew install bowtie bowtie2 bwa samtools igvtools
-brew install tophat cufflinks stringtie hisat2
-brew install seqtk minimap2 gfatools 
-brew install sratoolkit
+brew install lastz diamond paml # raxml
+brew install raxml --without-open-mpi 
+brew install fasttree newick-utils trimal
+brew install bowtie bowtie2 bwa samtools # igvtools
+brew install stringtie hisat2 # tophat cufflinks 
+brew install seqtk minimap2 # gfatools
+brew install sratoolkit edirect
 brew install genometools
-brew install canu
+brew install canu fastqc picard-tools kat
 brew install snp-sites --build-from-source # macOS bottles broken
 brew install bcftools
+
+# brew unlink proj
+brew install blast
 
 echo "==> Custom tap"
 brew tap wang-q/tap
 brew install faops multiz sparsemem intspan
-brew install jrunlist jrange
+# brew install jrunlist jrange
 
 echo "==> circos"
 brew install wang-q/tap/circos@0.69.9
 
 echo "==> RepeatMasker"
-brew install blast
-# brew unlink blast
 brew install hmmer easel
 
 brew install wang-q/tap/rmblast@2.10.0
-brew link rmblast@2.10.0 --overwrite
+# brew link rmblast@2.10.0 --overwrite
 
-# https://stackoverflow.com/questions/57629010/linuxbrew-curl-certificate-issue
-export HOMEBREW_CURLRC=1
-echo "--ciphers DEFAULT@SECLEVEL=1" >> $HOME/.curlrc
-brew install brewsci/bio/trf
+brew install wang-q/tap/trf@4
 
 brew install wang-q/tap/repeatmasker@4.1.1
 
@@ -52,7 +52,7 @@ pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple h5py
 cd $(brew --prefix)/Cellar/repeatmasker@4.1.1/4.1.1/libexec
 perl configure \
     -hmmer_dir=$(brew --prefix)/bin \
-    -rmblast_dir=$(brew --prefix)/bin \
+    -rmblast_dir=$(brew --prefix)/Cellar/rmblast@2.10.0/2.10.0/bin \
     -libdir=$(brew --prefix)/Cellar/repeatmasker@4.1.1/4.1.1/libexec/Libraries \
     -trf_prgm=$(brew --prefix)/bin/trf \
     -default_search_engine=rmblast
