eaabbeb660f786cf1d5688fed771057f809ece24
Qiang Wang <wang-q@outlook.com>
2022-04-09 15:39:03 +0800

    Use more curl

--- download.sh
+++ download.sh
@@ -22,7 +22,7 @@ else
 fi
 
 # Clone or pull other repos
-for OP in dotfiles alignDB withncbi; do
+for OP in dotfiles withncbi; do
     if [[ ! -d "$HOME/Scripts/$OP/.git" ]]; then
         if [[ ! -d "$HOME/Scripts/$OP" ]]; then
             echo "==> Clone $OP"
@@ -39,69 +39,64 @@ for OP in dotfiles alignDB withncbi; do
 done
 
 # alignDB
-chmod +x $HOME/Scripts/alignDB/alignDB.pl
-ln -fs $HOME/Scripts/alignDB/alignDB.pl $HOME/bin/alignDB.pl
+# chmod +x $HOME/Scripts/alignDB/alignDB.pl
+# ln -fs $HOME/Scripts/alignDB/alignDB.pl $HOME/bin/alignDB.pl
 
 echo "==> Jim Kent bin"
 cd $HOME/bin/
 RELEASE=$( ( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1 )
 if [[ $(uname) == 'Darwin' ]]; then
-    JKBIN_TAR_GZ=/tmp/jkbin-egaz-darwin-2011.tar.gz
-    if [ ! -e ${JKBIN_TAR_GZ} ]; then
-        wget -N -P /tmp https://github.com/wang-q/ubuntu/releases/download/20190906/jkbin-egaz-darwin-2011.tar.gz
-    fi
+    curl -L https://github.com/wang-q/ubuntu/releases/download/20190906/jkbin-egaz-darwin-2011.tar.gz
 else
     if echo ${RELEASE} | grep CentOS > /dev/null ; then
-        JKBIN_TAR_GZ=/tmp/jkbin-egaz-centos-7-2011.tar.gz
-        if [ ! -e ${JKBIN_TAR_GZ} ]; then
-            wget -N -P /tmp https://github.com/wang-q/ubuntu/releases/download/20190906/jkbin-egaz-centos-7-2011.tar.gz
-        fi
+        curl -L https://github.com/wang-q/ubuntu/releases/download/20190906/jkbin-egaz-centos-7-2011.tar.gz
     else
-        JKBIN_TAR_GZ=/tmp/jkbin-egaz-ubuntu-1404-2011.tar.gz
-        if [ ! -e ${JKBIN_TAR_GZ} ]; then
-            wget -N -P /tmp https://github.com/wang-q/ubuntu/releases/download/20190906/jkbin-egaz-ubuntu-1404-2011.tar.gz
-        fi
+        curl -L https://github.com/wang-q/ubuntu/releases/download/20190906/jkbin-egaz-ubuntu-1404-2011.tar.gz
     fi
-fi
+fi \
+    > jkbin.tar.gz
 
-echo "==> untar from ${JKBIN_TAR_GZ}"
-tar xvfz ${JKBIN_TAR_GZ} x86_64/axtChain
-tar xvfz ${JKBIN_TAR_GZ} x86_64/axtSort
-tar xvfz ${JKBIN_TAR_GZ} x86_64/axtToMaf
-tar xvfz ${JKBIN_TAR_GZ} x86_64/chainAntiRepeat
-tar xvfz ${JKBIN_TAR_GZ} x86_64/chainMergeSort
-tar xvfz ${JKBIN_TAR_GZ} x86_64/chainNet
-tar xvfz ${JKBIN_TAR_GZ} x86_64/chainPreNet
-tar xvfz ${JKBIN_TAR_GZ} x86_64/chainSplit
-tar xvfz ${JKBIN_TAR_GZ} x86_64/chainStitchId
-tar xvfz ${JKBIN_TAR_GZ} x86_64/faToTwoBit
-tar xvfz ${JKBIN_TAR_GZ} x86_64/lavToPsl
-tar xvfz ${JKBIN_TAR_GZ} x86_64/netChainSubset
-tar xvfz ${JKBIN_TAR_GZ} x86_64/netFilter
-tar xvfz ${JKBIN_TAR_GZ} x86_64/netSplit
-tar xvfz ${JKBIN_TAR_GZ} x86_64/netSyntenic
-tar xvfz ${JKBIN_TAR_GZ} x86_64/netToAxt
+echo "==> untar from jkbin.tar.gz"
+tar xvfz jkbin.tar.gz x86_64/axtChain
+tar xvfz jkbin.tar.gz x86_64/axtSort
+tar xvfz jkbin.tar.gz x86_64/axtToMaf
+tar xvfz jkbin.tar.gz x86_64/chainAntiRepeat
+tar xvfz jkbin.tar.gz x86_64/chainMergeSort
+tar xvfz jkbin.tar.gz x86_64/chainNet
+tar xvfz jkbin.tar.gz x86_64/chainPreNet
+tar xvfz jkbin.tar.gz x86_64/chainSplit
+tar xvfz jkbin.tar.gz x86_64/chainStitchId
+tar xvfz jkbin.tar.gz x86_64/faToTwoBit
+tar xvfz jkbin.tar.gz x86_64/lavToPsl
+tar xvfz jkbin.tar.gz x86_64/netChainSubset
+tar xvfz jkbin.tar.gz x86_64/netFilter
+tar xvfz jkbin.tar.gz x86_64/netSplit
+tar xvfz jkbin.tar.gz x86_64/netSyntenic
+tar xvfz jkbin.tar.gz x86_64/netToAxt
 
 mv $HOME/bin/x86_64/* $HOME/bin/
+rm jkbin.tar.gz
 
 if [[ $(uname) == 'Darwin' ]]; then
-    wget -N http://hgdownload.soe.ucsc.edu/admin/exe/macOSX.x86_64/faToTwoBit
-    chmod +x faToTwoBit
+    curl -L http://hgdownload.soe.ucsc.edu/admin/exe/macOSX.x86_64/faToTwoBit
 else
-    wget -N http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/faToTwoBit
-    chmod +x faToTwoBit
-fi
+    curl -L http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/faToTwoBit
+fi \
+    > faToTwoBit
 mv faToTwoBit $HOME/bin/
+chmod +x $HOME/bin/faToTwoBit
 
 echo "==> TrimGalore"
-curl -O https://raw.githubusercontent.com/FelixKrueger/TrimGalore/master/trim_galore
+curl -LO https://raw.githubusercontent.com/FelixKrueger/TrimGalore/master/trim_galore
 mv trim_galore $HOME/bin
 chmod +x $HOME/bin/trim_galore
 
 echo "==> circos-tools"
-wget -N -P /tmp http://circos.ca/distribution/circos-tools-0.22.tgz
-
 cd $HOME/share/
+curl -LO http://circos.ca/distribution/circos-tools-0.22.tgz
+
 rm -fr circos-tools
-tar xvfz /tmp/circos-tools-0.22.tgz
+tar xvfz circos-tools-0.22.tgz
 mv circos-tools-0.22 circos-tools
+
+rm circos-tools-0.22.tgz
