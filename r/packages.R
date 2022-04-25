9cf33907b107888b0ac815bcf5b8237c51390c1a
wang-q <wang-q@outlook.com>
2022-04-26 00:16:30 +0800

    `Ncpus = 6` halted the process

--- r/packages.R
+++ r/packages.R
@@ -8,7 +8,7 @@ is_installed <- function(package) {
 }
 
 # CRAN packages
-basic_libs <- c("devtools", "tidyverse", "reshape2", "getopt", "foreach", "doParallel", "tidyverse")
+basic_libs <- c("devtools", "reshape2", "getopt", "foreach", "doParallel", "tidyverse")
 graphics_libs <- c("scales", "gridExtra", "knitr", "rmarkdown", "kableExtra", "extrafont", "tikzDevice", "pander")
 stat_libs <- c("survival", "randomForestSRC", "pROC", "verification", "timeROC", "survminer", "VennDiagram", "vcd")
 bio_libs <- c("BiocManager", "ape", "adephylo", "genetics", "poppr", "taxize", "brranching")
@@ -16,7 +16,7 @@ misc_libs <- c("conquer", "covr", "deldir", "gmodels", "LearnBayes", "openxlsx",
 
 for(library in c(basic_libs, graphics_libs, stat_libs, bio_libs, misc_libs ) ) {
     if(!is_installed(library)) {
-        install.packages(library, repos="https://mirrors4.tuna.tsinghua.edu.cn/CRAN", Ncpus = 6)
+        install.packages(library, repos="https://mirrors4.tuna.tsinghua.edu.cn/CRAN")
     }
 }
 BiocManager::install(version = "3.14", ask = FALSE)
