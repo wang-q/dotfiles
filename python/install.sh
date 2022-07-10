762baf39a95dfff0ac84c43c6d9347e7ea75744f
Qiang Wang <wang-q@outlook.com>
2022-07-10 13:56:08 +0800

    Update install.sh

--- python/install.sh
+++ python/install.sh
@@ -12,18 +12,23 @@ PYPI_MIRROR=https://pypi.tuna.tsinghua.edu.cn/simple
 pip3 install -i ${PYPI_MIRROR} pysocks cryptography
 pip3 install -i ${PYPI_MIRROR} more-itertools zipp setuptools-scm
 pip3 install -i ${PYPI_MIRROR} numpy matplotlib
-pip3 install -i ${PYPI_MIRROR} tqdm tables networkx dataclasses tabulate
-pip3 install -i ${PYPI_MIRROR} plotly gmpy2 colorlover bokeh
-pip3 install -i ${PYPI_MIRROR} pandas sympy
-pip3 install -i ${PYPI_MIRROR} jupyter scipy
+pip3 install -i ${PYPI_MIRROR} pandas scipy jupyter sympy
 pip3 install -i ${PYPI_MIRROR} lxml statsmodels patsy
 pip3 install -i ${PYPI_MIRROR} beautifulsoup4 scikit-learn seaborn
+
+# PPanGGOLiN
+pip3 install -i ${PYPI_MIRROR} tqdm tables networkx dataclasses
+pip3 install -i ${PYPI_MIRROR} plotly gmpy2 colorlover bokeh
+
+# antismash
+pip3 install -i ${PYPI_MIRROR} helperlibs jinja2 joblib jsonschema markupsafe pysvg pyscss
+pip3 install -i ${PYPI_MIRROR} biopython bcbio-gff
 
 pip3 install -i ${PYPI_MIRROR} Circle-Map cutadapt importlib-metadata
 
 pip3 install -i ${PYPI_MIRROR} py2bit pyBigWig pysam deeptools
 
-pip3 install -i ${PYPI_MIRROR} h5py TBB bcbio-gff
+pip3 install -i ${PYPI_MIRROR} h5py TBB tabulate
 
 # poetry can search packages
 # curl -sSL https://install.python-poetry.org | python3 -
