dbf319731a0f76f4ef976932b5650035683c368f
wang-q <wang-q@outlook.com>
2023-05-15 04:52:30 +0800

    Update build.sh

--- r/build.sh
+++ r/build.sh
@@ -2,34 +2,60 @@
 
 BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# source URI
-sudo cp /etc/apt/sources.list /etc/apt/sources.list~
-sudo sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
-sudo sed -i 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+RELEASE=$( ( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1 )
+if [[ $(uname) == 'Darwin' ]]; then
+    # R itself
+    echo "Use the official build"
+    echo "https://mirrors.tuna.tsinghua.edu.cn/CRAN/bin/macosx/big-sur-arm64/base/R-4.2.2-arm64.pkg"
 
-sudo apt -y update
+    # packages
+    brew install cmake
+    brew install udunits
+    #    brew install gdal # Huge
+    exit
+else
+    if echo ${RELEASE} | grep CentOS > /dev/null ; then
+        # Manually
+        echo "You should build all items manually under CentOS"
+        exit
+    else
+        # source URI
+        sudo cp /etc/apt/sources.list /etc/apt/sources.list~
+        sudo sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
+        sudo sed -i 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+
+        sudo apt -y update
+
+        sudo apt -y install build-essential
+        sudo apt -y install gfortran
+
+        sudo apt -y install groff-base libxml2-dev gettext
+        sudo apt -y install libblas-dev liblapack-dev
+        sudo apt -y install libcurl4-openssl-dev libncurses5-dev libreadline-dev
+        sudo apt -y install libcairo2-dev libjpeg-dev libpango1.0-dev libpng-dev libtiff5-dev
+        sudo apt -y install libbz2-dev liblzma-dev zlib1g-dev
+
+        sudo apt -y install ghostscript libfreetype-dev fontconfig
+
+        # udunit2, gdal
+        sudo apt -y install cmake
+        sudo apt -y install udunits-bin libudunits2-dev
+        sudo apt -y install gdal-bin libgdal-dev
+
+        # brewed binaries confuse configure
+        hash brew 2>/dev/null && {
+            brew unlink pkg-config
+        }
+
+    fi
+fi
 
 # This command will install texlive
 # sudo apt -y build-dep r-base
 
-sudo apt -y install build-essential
-sudo apt -y install gfortran
-
 # sudo apt install apt-rdepends
 # apt-rdepends --build-depends --print-state --follow=DEPENDS r-base
 
-sudo apt -y install groff-base libxml2-dev gettext
-sudo apt -y install libblas-dev liblapack-dev
-sudo apt -y install libcurl4-openssl-dev libncurses5-dev libreadline-dev
-sudo apt -y install libcairo2-dev libjpeg-dev libpango1.0-dev libpng-dev libtiff5-dev
-sudo apt -y install libbz2-dev liblzma-dev zlib1g-dev
-
-sudo apt -y install ghostscript libfreetype-dev fontconfig
-
-# udunit2, gdal
-sudo apt -y install cmake
-sudo apt -y install udunits-bin libudunits2-dev
-sudo apt -y install gdal-bin libgdal-dev
 
 mkdir -p $HOME/share/R
 
@@ -38,12 +64,8 @@ curl -L https://mirrors.tuna.tsinghua.edu.cn/CRAN/src/base/R-4/R-4.2.1.tar.gz |
     tar xvz
 cd R-4.2.1
 
-# brewed binaries confuse configure
-hash brew 2>/dev/null && {
-    brew unlink pkg-config
-}
-
-CC=gcc CXX=g++ FC=gfortran ./configure \
+CC=gcc CXX=g++ FC=gfortran \
+./configure \
     --prefix="$HOME/share/R" \
     --enable-memory-profiling \
     --disable-java \
@@ -77,10 +99,12 @@ if grep -q -i R_42_PATH $HOME/.bashrc; then
     echo "==> .bashrc already contains R_42_PATH"
 else
     echo "==> Updating .bashrc with R_42_PATH..."
-    R_42_PATH="export PATH=\"$HOME/share/R/bin:\$PATH\""
+    R_42_PATH="export PATH=\"\$HOME/share/R/bin:\$PATH\""
     echo '# R_42_PATH' >> $HOME/.bashrc
     echo $R_42_PATH    >> $HOME/.bashrc
     echo >> $HOME/.bashrc
+
+    eval $R_42_PATH
 fi
 
 source ~/.bashrc
