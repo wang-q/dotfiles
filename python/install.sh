18204237bf489119ff6c55177d98923e00c2e2c9
Qiang Wang <wang-q@outlook.com>
2023-06-11 10:10:43 +0800

    Update install.sh

--- python/install.sh
+++ python/install.sh
@@ -5,7 +5,7 @@ BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "${BASE_DIR}" || exit
 
 # pip
-PYPI_MIRROR=https://mirrors.bfsu.edu.cn/pypi/web/simple
+PYPI_MIRROR=https://mirrors.nju.edu.cn/pypi/web/simple
 
 #pip3 install -i ${PYPI_MIRROR} --upgrade pip setuptools
 
